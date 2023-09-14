de89feeb035196994f6bf49e019d31f365ac4817
0KiRa1 <aarjavjain035@gmail.com>
2023-09-14 10:25:42 +0530

    update

--- app/page.js
+++ app/page.js
@@ -119,17 +119,48 @@
 
 // -----------React Toastify--------------
 
+// "use client"
+// import React from 'react'
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+// const page = () => {
+//   const notify = ()=>{
+//     toast.success('🦄 Wow so easy!', {
+//       position: "top-center",
+//       autoClose: 5000,
+//       hideProgressBar: false,
+//       closeOnClick: true,
+//       pauseOnHover: true,
+//       draggable: true,
+//       progress: undefined,
+//       theme: "dark",
+//       });
+//   }
+//   return (
+//     <div className='p-20'>
+//       <button onClick={notify} className='px-6 py-2 bg-green-500 text-white font-bold rounded'>Login</button>
+//       <ToastContainer/>
+//     </div>
+//   )
+// }
+
+// export default page
+
+
+
+//  -----------Server side rendering----------
+
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <button className='px-6 py-2 bg-green-500 text-white font-bold rounded'>Get</button>
+      Home
     </div>
   )
 }
 
 export default page
-
 
 
